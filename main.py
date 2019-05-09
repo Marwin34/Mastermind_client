983cbc29cab97a4d06c6dbd4154cd95474f95962
Marwin34 <marcinwojsik@wp.pl>
2019-05-10 01:29:36 +0200

    Add some small changes.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import communication
 import time
 import pygame
-import input
+import gui
 
 
 class Main:
@@ -11,16 +11,22 @@ class Main:
 
         self.size = self.width, self.height = [800, 600]
 
-        self.Display = pygame.Surface
+        self.display = pygame.Surface
 
-        self.screen = None
         self.communication_module = None
 
         self.time_stamp = 0.016  # 60 FPS
 
+        self.gui = None
+
+        self.picked_colors = [None, None, None, None]
+
     def run(self):
         pygame.init()
-        self.screen = pygame.display.set_mode(size=self.size)
+        self.display = pygame.display.set_mode(size=self.size)
+
+        self.gui = gui.GUI(self.display)
+
         self.communication_module = communication.ComSupervisor("127.0.0.1", 50001)
 
         if not self.communication_module.connect():
@@ -33,15 +39,7 @@ class Main:
 
                 self.handle_events()
 
-                if self.connected:
-                    data = {
-                        'type': 'hi',
-                        'value': 'test'
-                    }
-
-                    self.communication_module.send(data)
-
-                self.screen.fill((0, 0, 0))
+                self.gui.update("IN-GAME", self.process_input)
                 pygame.display.flip()
 
                 # print(1 / (time.perf_counter() - last_update))
@@ -53,6 +51,23 @@ class Main:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+
+    def process_input(self, inputs):
+        self.picked_colors = inputs['picked_colors']
+
+        if inputs['send']:
+            self.send()
+
+    def send(self):
+        if self.connected:
+            data = {
+                'type': 'colors_match',
+                'value': self.picked_colors
+            }
+
+            self.communication_module.send(data)
+        else:
+            print("Disconnected.")
 
 
 if __name__ == '__main__':
